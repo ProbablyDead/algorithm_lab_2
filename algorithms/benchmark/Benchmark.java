1dbeb78c827b5ed39347c409070ad077e2b15398
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-04-26 16:40:26 +0300

    add generator

--- algorithms/benchmark/Benchmark.java
+++ algorithms/benchmark/Benchmark.java
@@ -7,7 +7,14 @@ import algorithms.solution.SecondAlgorithm;
 import algorithms.solution.ThirdAlgorithm;
 
 public class Benchmark {
+
   public static void main (String[] args) {
-    System.out.println(FirstAlgorithm.firstAlgorithmMain(new Scanner(System.in)));
+    Generator generator = new Generator();
+    
+    String data = generator.generateRectanglesString(5) + generator.generatePointsString(5);
+
+    System.out.println(FirstAlgorithm.firstAlgorithmMain(new Scanner(data)));
+    System.out.println(SecondAlgorithm.secondAlgorithmMain(new Scanner(data)));
+    System.out.println(ThirdAlgorithm.thirdAlgorithmMain(new Scanner(data)));
   }
 }
